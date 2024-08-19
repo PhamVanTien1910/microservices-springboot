c2c0a0392c31a6b9e7d194b41e1babf1c4b718be
unknown <tienphamvan2005@gmail.com>
2024-08-19 16:43:12 +0700

    microservices basic

--- api-gateway/src/main/java/com/tienpv/gateway/repository/IdentityClient.java
+++ api-gateway/src/main/java/com/tienpv/gateway/repository/IdentityClient.java
@@ -3,14 +3,12 @@ package com.tienpv.gateway.repository;
 import com.tienpv.gateway.dto.request.IntrospectRequest;
 import com.tienpv.gateway.dto.response.ApiResponse;
 import com.tienpv.gateway.dto.response.IntrospectResponse;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.PostExchange;
 import reactor.core.publisher.Mono;
 
 public interface IdentityClient {
-
     @PostExchange(value = "/auth/introspect", contentType = MediaType.APPLICATION_JSON_VALUE)
     Mono<ApiResponse<IntrospectResponse>> introspect(@RequestBody IntrospectRequest request);
 }
